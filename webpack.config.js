49eab500e58afc5610cfde62e40d529385dac8f4
deokgi kim <dukgi77@naver.com>
2022-07-18 10:41:36 +0900

    Feat: :sparkles: Webpack 적용 (3) - style(css, sass ...) loader
    1. webpack 은 js 밖에 모르기 때문에 css 를 js 로 변환해서 로딩해야 한다.
    2. style-loader 를 이용해서 js 로 변경된 css 를 동적으로 head태그에 추가할 수 있다. 하지만 파일마다 추가하기 때문에 한번에 추가하기 위해서 MiniCssExtractPlugin 을 쓴다.
    3.sass-loader 는 .scss, .sass 파일을 import 하게 해준다. 그리고 css 파일로 변환시킨다.
    4. node-sass 는 css 컴파일을 매우 빠르게 시켜준다 라고 한다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 const config = {
@@ -12,9 +13,18 @@ const config = {
         test: /\.js$/,
         use: "babel-loader",
         exclude: /node_modules/,
-      }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    })
+  ]
 };
 
 module.exports = config;
